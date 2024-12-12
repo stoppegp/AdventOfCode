356a7c6ae65fd255df1974537918dfa67ecf6ebf
Stoppler, Martin (TD-G) <martin.stoppler@iav.de>
2024-12-12 07:42:22 +0100

    simplify 2024/11

--- adventofcode/2024/11/11.py
+++ adventofcode/2024/11/11.py
@@ -14,87 +14,32 @@ def blink_one(stone):
     else:
         return [stone * 2024]
 
-@cache
-def blink(stones):
-    new_stones = []
-    for stone in stones:
-        new_stones.extend(blink_one(stone))
-    return tuple(new_stones)
 
-def count_on(current, next_stones):
+def count_on(current):
     #ret = {x: 0 for x in next_stones.keys()}
     ret = {}
     for stone, ct in current.items():
-        if stone in next_stones.keys():
-            for st, ct2 in next_stones[stone].items():
-                if st not in ret.keys():
-                    ret[st] = 0
-                ret[st] += ct*ct2
-        else:
-            new_stones = blink_one(stone)
-            #next_stones[stone] = {}
-            for new_stone in new_stones:
-                if new_stone not in ret.keys():
-                    ret[new_stone] = 0
-                ret[new_stone] += 1*ct
-                #if new_stone not in next_stones[stone].keys():
-                #    next_stones[stone][new_stone] = 0
-                #next_stones[stone][new_stone] += 1
+        new_stones = blink_one(stone)
+        for new_stone in new_stones:
+            if new_stone not in ret.keys():
+                ret[new_stone] = 0
+            ret[new_stone] += 1*ct
 
-    return ret, next_stones
+    return ret
 
 def part1(input, part):
-    stones = tuple([int(x) for x in input.split()])
-    #for stone in stones_base:
-    y0 = 0
-    x0 = 0
+    stones = [int(x) for x in input.split()]
+    stones_cur = {x: stones.count(x) for x in set(stones)}
 
     if part == 1:
         target_no = 25
     else:
         target_no = 75
 
-    final_no = 0
-
-
-    #stones = tuple([stone])
-    # for i in range(target_no):
-    #     print(f"initial scan: {i}")
-    #     stones_set = set(stones)
-    #     stones = blink(stones)
-    #     print(len(set(stones)))
-    #     if set(stones) == stones_set:
-    #         break
-    #
-    # if i == target_no-1:
-    #     final_no += len(stones)
-    #     return final_no
-    #
-    # next_stones = {x: {} for x in stones_set}
-    # for stone in stones_set:
-    #     new_stones = blink_one(stone)
-    #     for test_stone in stones_set:
-    #         next_stones[stone][test_stone] = new_stones.count(test_stone)
-    # #print(next_stones)
-    #
-    # current_no = i
-
-
-    current_no = 0
-    stones_cur = {x: stones.count(x) for x in set(stones)}
-    next_stones = {}
-    print(current_no)
-    for i in range(current_no, target_no):
+    for i in range(target_no):
         print(i)
-        print(stones_cur)
-        stones_cur, next_stones = count_on(stones_cur, next_stones)
-        print(next_stones)
-        print(sum(list(stones_cur.values())))
-        print()
+        stones_cur = count_on(stones_cur)
 
-    #f = interpolate.interp1d(x, y, kind='linear', fill_value="extrapolate")
-
-    #sum += round(10**f(check))
 
     return sum(list(stones_cur.values()))
 
