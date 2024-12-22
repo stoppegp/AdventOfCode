970bb00c33dc5b716c5c69b7943db54ce1935522
stoppegp <ich@stoppe-gp.de>
2024-12-22 07:13:11 +0100

    template update

---
+++ adventofcode/template/day.py
@@ -0,0 +1,13 @@
+from adventofcode.common import run, input_to_grid, input_to_lines
+import re
+
+
+def puzzle(input, part=1, example=False, *args, **kwargs):
+
+    return None
+
+
+
+if __name__ == '__main__':
+    run(cb=puzzle, example_file="example", solution_file="solution1")
+    #run(cb=puzzle, example_file="example", solution_file="solution2", part=2)
