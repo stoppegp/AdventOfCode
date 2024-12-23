f5f09d4e7cb0b2d8d8be7bc0efb01d0d41bb91c3
stoppegp <ich@stoppe-gp.de>
2024-12-23 06:40:03 +0100

    2024/23 improvements

--- adventofcode/2024/23/23.py
+++ adventofcode/2024/23/23.py
@@ -26,12 +26,13 @@ def puzzle(input, part=1, example=False, *args, **kwargs):
             if pc1 in all_pcs[pc0]:
                 for pc2 in all_pcs.keys():
                     if pc2 in all_pcs[pc1] and pc2 in all_pcs[pc0]:
-                        triplets.add(tuple(sorted([pc0, pc1, pc2])))
+                        triplets.add(frozenset([pc0, pc1, pc2]))
 
     def expand_clusters(clusters, all_pcs):
         new_clusters = set()
         for cluster in clusters:
-            for new_pc in all_pcs.keys():
+            possible_new_pcs = {y for x in cluster for y in all_pcs[x]}
+            for new_pc in possible_new_pcs:
                 if new_pc in cluster:
                     continue
                 add_to_cluster = True
@@ -40,26 +41,27 @@ def puzzle(input, part=1, example=False, *args, **kwargs):
                         add_to_cluster = False
                         break
                 if add_to_cluster:
-                    new_clusters.add(tuple(sorted([*cluster, new_pc])))
+                    new_clusters.add(frozenset([*cluster, new_pc]))
         return new_clusters
 
     if part == 1:
-        return len([(x, y, z) for x, y, z in triplets if x[0] == "t" or y[0] == "t" or z[0] == "t"])
+        return len([x for x in triplets if list(x)[0][0] == "t" or list(x)[1][0] == "t" or list(x)[2][0] == "t"])
 
     clusters = triplets
     while True:
         new_clusters = expand_clusters(clusters, all_pcs)
+        print(len(new_clusters))
         if len(new_clusters) > 0:
             clusters = new_clusters
         else:
             break
 
     if len(clusters) == 1:
-        return ",".join(list(clusters)[0])
+        return ",".join(sorted(list(clusters)[0]))
     else:
         raise Exception()
 
 
 if __name__ == '__main__':
-    run(cb=puzzle, example_file="example", solution_file="solution1")
+    #run(cb=puzzle, example_file="example", solution_file="solution1")
     run(cb=puzzle, example_file="example", solution_file="solution2", part=2)
